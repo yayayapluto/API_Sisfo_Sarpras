ab1432020dc3a2bc8948cc5a70aac9f99e8e16b4
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-04-30 21:32:03 +0700

    docs

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,9 @@ Route::prefix("auth")->group(function () {
 Route::middleware("need-token")->group(function () {
 
    Route::prefix("admin")->middleware("role:admin")->group(function () {
-      Route::apiResources([
-          "categories" => \App\Http\Controllers\CategoryController::class,
-          "racks" => \App\Http\Controllers\RackController::class,
-          "items" => \App\Http\Controllers\ItemController::class,
-      ]);
+      Route::apiResource("categories", \App\Http\Controllers\CategoryController::class);
+      Route::apiResource("racks", \App\Http\Controllers\RackController::class);
+      Route::apiResource("items", \App\Http\Controllers\ItemController::class);
 
       Route::apiResource("borrowings", \App\Http\Controllers\BorrowingController::class)->except(["store","update","destroy"]);
       Route::patch("borrowings/{id}/approve", [\App\Http\Controllers\BorrowingController::class, "approve"]);
@@ -35,26 +33,6 @@ Route::middleware("need-token")->group(function () {
        Route::apiResource("returns", \App\Http\Controllers\ReturningController::class)->only(["index","show","store"]);
    });
 
-});
-
-Route::prefix("testing")->group(function () {
-    Route::get("no-token", function () {
-        return \App\Custom\Formatter::apiResponse(200, "This route need no token");
-    });
-
-    Route::prefix("need-token")->middleware("need-token")->group(function () {
-        Route::get("all", function () {
-            return \App\Custom\Formatter::apiResponse(200, "This route for all role");
-        });
-
-        Route::middleware("role:admin")->get("admin", function () {
-            return \App\Custom\Formatter::apiResponse(200, "This route for admin only");
-        });
-
-        Route::middleware("role:user")->get("user", function () {
-            return \App\Custom\Formatter::apiResponse(200, "This route for user only");
-        });
-    });
 });
 
 Route::fallback(function () {
